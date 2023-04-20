1fbaadccd4c18aeee004a5d024ab82170f30b541
amitmohanta259@gmail.com <amit.mohanta@thinkitive.com>
2023-04-20 09:38:55 -0400

    verify pan

--- src/main/java/com/main/sundaylabs/designpattern/ViewProfile.java
+++ src/main/java/com/main/sundaylabs/designpattern/ViewProfile.java
@@ -1,9 +1,12 @@
 package com.main.sundaylabs.designpattern;
 
+import java.util.List;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.testng.Assert;
 
 public class ViewProfile{
 	WebDriver driver;
@@ -20,15 +23,54 @@ public class ViewProfile{
 	@FindBy(xpath = "//button/div[text()='Upload Documents']")
 	WebElement uploadDoc;
 	
-//	@FindBy(xpath = "//button[@class='custom-button ']//div[contains(text(),'Login')]")
-//	WebElement uploadDocument;
-//	
+	@FindBy(xpath = "//div[text()='PAN']//parent::div//following-sibling::div//div//div[2]")
+	WebElement panPopUpButton;
+	
+	@FindBy(xpath = "//div[text()='Verify PAN Card']")
+	WebElement popUpHeading;
+
+	@FindBy(xpath = "//input[@placeholder='Enter you PAN Card Number']")
+	WebElement panCardInputField;
+	
+	@FindBy(xpath = "(//button[@class='custom-button '])[3]")
+	WebElement verifyPanSubmitButton;
+	
+	@FindBy(xpath = "//div[@class='field-items modal-text']/p")
+	WebElement validationMessage;
 	
 	public void AccountInfo() throws InterruptedException{
 		profile.click();
 		viewProfile.click();
 		Thread.sleep(2000);
-		uploadDoc.click();
+		
 	}
+	
+	public Boolean VerifyPanCardPopWindow() throws InterruptedException {
+		Boolean status=false;
+		Thread.sleep(1000);
+		panPopUpButton.click();
+		Thread.sleep(1000);
+		if(popUpHeading.getText().equals("Verify PAN Card")) {
+			status=true;
+		}
+		return status;
+	}
+	
+	public String verifyPanCardWithInValidPan() throws InterruptedException {
+		panCardInputField.sendKeys("QWERT1234K");
+		Thread.sleep(1000);
+		verifyPanSubmitButton.click();
+		return validationMessage.getText();
+	}
+	
+	public String verifyPanCardWithValidPan() throws InterruptedException {
+		panCardInputField.sendKeys("ALWPG5809L");
+		Thread.sleep(1000);
+		verifyPanSubmitButton.click();
+		return validationMessage.getText();
+	}
+	
+	
+	
 	
 }
--- src/test/java/com/automation/sundaylabs/SundayLab/EndtoEndTest.java
+++ src/test/java/com/automation/sundaylabs/SundayLab/EndtoEndTest.java
@@ -1,31 +1,56 @@
 package com.automation.sundaylabs.SundayLab;
 import org.testng.annotations.Test;
+import org.testng.AssertJUnit;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 
 import com.main.sundaylabs.designpattern.*;
 import com.main.sundaylabs.library.AppLibrary;
 
+import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 
 
 
 public class EndtoEndTest {
+	ViewProfile v;
 	@BeforeTest
 	public void initialize() {
 		AppLibrary.initializeDriver();
 	}
-	@Test
+	@Test(priority = 1)
 	public void Login() throws InterruptedException {
 		Thread.sleep(1000);
 		Login l = new Login(AppLibrary.driver);
 		l.performLoginOperations();
 	}
 	
-	@Test
+	@Test(priority = 2)
 	public void ProfilePage() throws InterruptedException {
 		Thread.sleep(10000);
-		ViewProfile v = new ViewProfile(AppLibrary.driver);
+		v = new ViewProfile(AppLibrary.driver);
 		v.AccountInfo();
+	}
+	
+	@Test(priority = 3)
+	public void verifyUserType() throws InterruptedException {
+		Assert.assertTrue(v.VerifyPanCardPopWindow()); 
+	}
+	
+	@Test(priority = 4)
+		public void verifyPan() throws InterruptedException {
+			SoftAssert softAssert = new SoftAssert();
+			softAssert.assertEquals(v.verifyPanCardWithInValidPan(),"Failed to verify PAN, Please try again.","Title is not Matching");
+			Thread.sleep(1000);
+			softAssert.assertEquals(v.verifyPanCardWithValidPan(),"Failed to verify PAN, Please try again.","Title is not Matching");
+			softAssert.assertAll();
+			
+		}
+	
+	@Test(priority = 5)
+	public void verifyAdhar() throws InterruptedException{
+		
 	}
 	
 	
